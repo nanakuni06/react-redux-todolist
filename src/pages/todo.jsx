c6a507e4ed386983701b6db05411ed0916e54133
Nana <nanakurnianingsih82716@gmail.com>
2023-10-30 09:27:23 +0700

    first commit

---
+++ src/pages/todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoInput from '../components/todo-input'
+import TodoList from '../components/todo-list'
+
+function Todo() {
+  return (
+    <>
+        <TodoInput/>
+        <TodoList/>
+    </>
+  )
+}
+
+export default Todo
