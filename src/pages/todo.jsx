2fa7436e38fcec614dfa2a5c43195e351b93ef2e
Nana <nanakurnianingsih82716@gmail.com>
2023-10-30 18:14:21 +0700

    fix

--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -7,7 +7,7 @@ import FilterTodo from '../components/filter-todo'
 function Todo() {
   return (
     <>
-    <div className='lg:px-80 lg:py-20 mx-0 py-20 px-5 bg-gradient-to-r from-pink-500 to-violet-500'>
+    <div className='lg:px-80 lg:py-60 mx-0 py-20 px-5 bg-gradient-to-r from-pink-500 to-violet-500'>
       <div className="card shadow-2xl justify-center bg-violet-400">
         <div className="card-body">
             <Header/>
